18e47c67232a8ba9f1010916dc67090cddd97161
Dansterf <dvalcinor@gmail.com>
2025-12-29 13:04:24 -0500

    Change file serving method in simple_checkin_route

--- github-upload-flat/routes/simple_checkin_route.py
+++ github-upload-flat/routes/simple_checkin_route.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, send_file
+from flask import Blueprint, send_from_directory
 import os
 
 simple_checkin_bp = Blueprint("simple_checkin_bp", __name__)
@@ -6,6 +6,6 @@ simple_checkin_bp = Blueprint("simple_checkin_bp", __name__)
 @simple_checkin_bp.route("/simple-checkin", methods=["GET"])
 def simple_checkin_page():
     """Serve the simple check-in HTML page"""
-    # Get the path to the HTML file
-    html_path = os.path.join(os.path.dirname(__file__), 'static', 'checkin_simple.html')
-    return send_file(html_path)
+    # Get the static folder path
+    static_folder = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static')
+    return send_from_directory(static_folder, 'checkin_simple.html')
